eaffe0fe60e54d2d424a2dd8717a770fe231d6b9
SONAL SINGH <156330222+sonall99@users.noreply.github.com>
2026-01-17 08:06:07 +0530

    Update backendd.py

--- Backend/backendd.py
+++ Backend/backendd.py
@@ -110,10 +110,8 @@ async def handle_summarize_request(request: VideoRequest):
         raise HTTPException(status_code=500, detail=f"An internal error occurred: {str(e)}")
 
 if __name__ == "__main__":
-    print("Starting FastAPI server at http://127.0.0.1:8000")
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
 
-print("AVAILABLE MODELS:")
-for m in genai.list_models():
-    print(m.name)
+
